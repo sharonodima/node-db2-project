09e97c14b32736dc2b6c9bd03888421ba42bc4bf
Sharon Odima <odimasharon@gmail.com>
2022-06-17 08:24:56 -0400

    Sharon Odima Unit4.2Module2 Project Initial Commit

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [
+    {
+        vin: "JTHBK262X65020306",
+        make: "toyota",
+        model:"prius",
+        mileage: 215000,
+        title: "clean",
+        transmission: "manual",
+    },
+    {
+        vin: "1FADP3K25DL345399",
+        make: "toyota",
+        model:"corolla",
+        mileage: 115000,
+        title: "clean",
+    },
+    {
+        vin: "5TDKK4CC4AS293335",
+        make: "ford",
+        model:"focus",
+        mileage: 150000,
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex("cars").truncate().then(() => {
+//         return knex(cars).insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    return knex("cars")
+        .truncate().then(() => {
+            return knex("cars").insert(cars)
+        })
+}
